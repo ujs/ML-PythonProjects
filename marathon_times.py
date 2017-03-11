7f82c1a859eaee77d66cda0d5499ed311a36134b
ujs <ujalashanker@gmail.com>
2017-03-11 15:48:31 +0530

    data load

--- marathon_times.py
+++ marathon_times.py
@@ -5,3 +5,5 @@ import seaborn as sns
 sns.set()
 
 run_data = pd.read_csv('marathon-data.csv')
+run_data['split'] = pd.to_timedelta(run_data['split'])
+run_data['final'] = pd.to_timedelta(run_data['final'])
