1d495dd47785922d0d48c31d2fc8d74c15fd2460
ujs <ujalashanker@gmail.com>
2017-03-11 16:11:33 +0530

    split strategy analysis

--- marathon_times.py
+++ marathon_times.py
@@ -7,3 +7,11 @@ sns.set()
 run_data = pd.read_csv('marathon-data.csv')
 run_data['split'] = pd.to_timedelta(run_data['split'])
 run_data['final'] = pd.to_timedelta(run_data['final'])
+
+run_data['split_sec'] = run_data['split'].astype(int)/1E9
+run_data['final_sec'] = run_data['final'].astype(int)/1E9
+ # Split strategy calculates if runner had a negative split or positive split during the race
+ # A negative value of 'split_strategy' means negative split and positive value means positive split
+run_data['split_strategy'] = run_data['final_sec'] - 2* run_data['split_sec']
+
+sns.distplot(run_data['split_strategy'], kde = 'False')
