86b72d863276c558c3a35c45f6077ab6f1a7d6b0
ujs <ujalashanker@gmail.com>
2017-03-13 12:15:55 +0530

    feature vectorization

--- vectorization.py
+++ vectorization.py
@@ -5,14 +5,14 @@ import matplotlib.pyplot as plt
 
 # #Categorical Data
 
-# data = [{'price': 850000, 'rooms': 4, 'neighborhood': 'Queen Anne'},
-#     {'price': 700000, 'rooms': 3, 'neighborhood': 'Fremont'},
-#     {'price': 650000, 'rooms': 3, 'neighborhood': 'Wallingford'},
-#     {'price': 600000, 'rooms': 2, 'neighborhood': 'Fremont'}]
+data = [{'price': 850000, 'rooms': 4, 'neighborhood': 'Queen Anne'},
+    {'price': 700000, 'rooms': 3, 'neighborhood': 'Fremont'},
+    {'price': 650000, 'rooms': 3, 'neighborhood': 'Wallingford'},
+    {'price': 600000, 'rooms': 2, 'neighborhood': 'Fremont'}]
 
-# from sklearn.feature_extraction import DictVectorizer
-# vec = DictVectorizer(sparse=True, dtype=int)
-# vec.fit_transform(data) 
+from sklearn.feature_extraction import DictVectorizer
+vec = DictVectorizer(sparse=True, dtype=int)
+vec.fit_transform(data) 
 
 
 
@@ -23,21 +23,21 @@ import matplotlib.pyplot as plt
 
 # #Approach 1- CountVectorizer
 
-# sample = ['problem of evil','evil queen','horizon problem']
+sample = ['problem of evil','evil queen','horizon problem']
 
-# from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
 
-# vec = CountVectorizer()
-# X = vec.fit_transform(sample)
+vec = CountVectorizer()
+X = vec.fit_transform(sample)
 
-# pd.DataFrame(X.toarray(), columns=vec.get_feature_names())
+pd.DataFrame(X.toarray(), columns=vec.get_feature_names())
 
 
 # #Approach 2- TfidfVectorizer
-# from sklearn.feature_extraction.text import TfidfVectorizer
-# vec = TfidfVectorizer()
-# X = vec.fit_transform(sample)
-# pd.DataFrame(X.toarray(), columns=vec.get_feature_names())
+from sklearn.feature_extraction.text import TfidfVectorizer
+vec = TfidfVectorizer()
+X = vec.fit_transform(sample)
+pd.DataFrame(X.toarray(), columns=vec.get_feature_names())
 
 
 
@@ -45,30 +45,33 @@ import matplotlib.pyplot as plt
 
 # #Trial (different transforms)
 
-# x = np.array([3,5,2,6,4,2])
+x = np.array([3,5,2,6,4,2])
 
-# Y_trial = np.array([5,2,6,8,3,5])
+Y_trial = np.array([5,2,6,8,3,5])
 
 
 
-# from sklearn.linear_model import LinearRegression
-# X_trial = x[:,np.newaxis]
-# X_test = X_trial
-# model = LinearRegression()
-# Y_model = model.fit(X_trial,Y_trial).predict(X_test)
+from sklearn.linear_model import LinearRegression
+X_trial = x[:,np.newaxis]
+X_test = X_trial
+model = LinearRegression()
+Y_model = model.fit(X_trial,Y_trial).predict(X_test)
 # plt.scatter(X_trial,Y_trial)
 # plt.plot(X_test,Y_model)
 
 
-# from sklearn.preprocessing import PolynomialFeatures
-# tran = PolynomialFeatures(degree=3, include_bias=False)
-# X_new = tran.fit_transform(X_trial)
+from sklearn.preprocessing import PolynomialFeatures
+tran = PolynomialFeatures(degree=3, include_bias=False)
+X_new = tran.fit_transform(X_trial)
 
-# Y_model = model.fit(X_new,Y_trial).predict(X_new)
-# # plt.scatter(x,Y_trial)
-# # plt.plot(x,Y_model)
+Y_model = model.fit(X_new,Y_trial).predict(X_new)
+# plt.scatter(x,Y_trial)
+# plt.plot(x,Y_model)
 
+
+# Shortcut using pipeline
 from numpy import nan
+from sklearn.preprocessing import Imputer
 X = np.array([[ nan, 0,   3  ],
               [ 3,   7,   9  ],
               [ 3,   5,   2  ],
