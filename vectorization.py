eca735835de984d7a4ee82ec0a27c38c97e7a018
ujs <ujalashanker@gmail.com>
2017-03-13 10:54:55 +0530

    feature vectorization

--- vectorization.py
+++ vectorization.py
@@ -1,8 +1,43 @@
+
+import pandas as pd
+
+#Categorical Data
+
 data = [{'price': 850000, 'rooms': 4, 'neighborhood': 'Queen Anne'},
     {'price': 700000, 'rooms': 3, 'neighborhood': 'Fremont'},
     {'price': 650000, 'rooms': 3, 'neighborhood': 'Wallingford'},
     {'price': 600000, 'rooms': 2, 'neighborhood': 'Fremont'}]
 
 from sklearn.feature_extraction import DictVectorizer
-vec = DictVectorizer(sparse=False, dtype=int)
+vec = DictVectorizer(sparse=True, dtype=int)
 vec.fit_transform(data) 
+
+
+
+
+
+
+#Text
+
+#Approach 1- CountVectorizer
+
+sample = ['problem of evil','evil queen','horizon problem']
+
+from sklearn.feature_extraction.text import CountVectorizer
+
+vec = CountVectorizer()
+X = vec.fit_transform(sample)
+
+pd.DataFrame(X.toarray(), columns=vec.get_feature_names())
+
+
+#Approach 2- TfidfVectorizer
+vec = TfidfVectorizer()
+X = vec.fit_transform(sample)
+pd.DataFrame(X.toarray(), columns=vec.get_feature_names())
+
+
+
+
+
+#Images
