dab72c39f021582a67da67794a6e34817ad96f30
ujs <ujalashanker@gmail.com>
2017-03-13 22:39:18 +0530

    Gaussian NB

--- GaussianNB.py
+++ GaussianNB.py
@@ -27,3 +27,29 @@ from sklearn.datasets import fetch_20newsgroups
 
 data = fetch_20newsgroups()
 data.target_names
+
+categories = ['talk.religion.misc', 'soc.religion.christian',
+              'sci.space', 'comp.graphics']
+train = fetch_20newsgroups(subset='train', categories=categories)
+test = fetch_20newsgroups(subset='test', categories=categories)
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import make_pipeline
+
+model = make_pipeline(TfidfVectorizer(),MultinomialNB())
+labels = model.fit(train.data,train.target).predict(test.data)
+
+#Using confusion matrix to compare true vs predicted labels
+from sklearn.metrics import confusion_matrix
+mat = confusion_matrix(test.target, labels)
+sns.heatmap(mat.T, square=True, annot=True, fmt='d', cbar=False,
+            xticklabels=train.target_names, yticklabels=train.target_names)
+plt.xlabel('true label')
+plt.ylabel('predicted label')
+
+
+#Function to predict category of news based on a phrase/sentence
+def predict_category(s, train=train, model=model):
+    pred = model.predict([s])
+    return train.target_names[pred[0]]
