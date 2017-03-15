0118e1c69656a34f2326cd748e164787d13cad37
ujs <ujalashanker@gmail.com>
2017-03-15 20:37:09 +0530

    Regression

--- RegressionPredictTraffic.py
+++ RegressionPredictTraffic.py
@@ -9,3 +9,14 @@ weather =pd.read_csv('weatherSeattle.csv', index_col = 'DATE', parse_dates = Tru
 data = counts.resample('d', how='sum')
 data['Total'] = data.sum(axis =1)
 data = data['Total']
+
+# days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+# for i in range(7):
+# 	data[days[i]] = (data.index.day == i).astype(float)
+
+from pandas.tseries.holiday import USFederalHolidayCalendar
+cal = USFederalHolidayCalendar()
+holidays = cal.holidays('2012', '2016')
+holidays = pd.Series(1, index = holidays, name='holiday')
+data = data.join(holidays)
+data['holiday'].fillna(0, inplace=True)
