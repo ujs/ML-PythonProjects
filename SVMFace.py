a79bcfa60d69c028d0ed912164d48cdb15f0ae6a
ujs <ujalashanker@gmail.com>
2017-03-17 11:09:39 +0530

    SVM face recognition

--- SVMFace.py
+++ SVMFace.py
@@ -12,15 +12,28 @@ for i, axi in enumerate(ax.flat):
 	axi.set(xticks = [], yticks = [], xlabel = faces.target_names[faces.target[i]])
 
 #Each Image has 3K pixels. So will use PCA to extract main 150 features
-#Importing libraries (Classes)
+#Importing libraries (Classes) for SVM, PCA and pipelines
 
 from sklearn.svm import SVC
 from sklearn.decomposition import RandomizedPCA
 from sklearn.pipeline import make_pipeline
 
+#Instantiating the imported classes
+
 pca = RandomizedPCA(n_components=150, whiten=True, random_state=42)
 svc = SVC(kernel='rbf', class_weight='balanced')
 model = make_pipeline(pca, svc)
 
+#preparing pur training and test data
+
 from sklearn.cross_validation import train_test_split
 Xtrain, Xtest, ytrain, ytest = train_test_split(faces.data, faces.target, random_state=42)
+
+#Comparing the performance of the model for different parameters(C- margin hardness, gamma- size of the radial basis function kernel)
+from sklearn.grid_search import GridSearchCV
+param_grid = {'svc__C': [1, 5, 10, 50],
+              'svc__gamma': [0.0001, 0.0005, 0.001, 0.005]}
+grid = GridSearchCV(model, param_grid)
+
+grid.fit(Xtrain, ytrain)
+print(grid.best_params_)
