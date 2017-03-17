493c71717e87a82f9af005e3a4206d96052bdd17
ujs <ujalashanker@gmail.com>
2017-03-17 16:17:47 +0530

    clustering

--- clustering.py
+++ clustering.py
@@ -6,9 +6,17 @@ import seaborn as sns
 
 # Load Data
 from sklearn.datasets import load_digits
-digits = load_digits
 
-#Clustering
+
+# Clustering
 
 from sklearn.cluster import KMeans
-clusters = kmeans.fit_predict(digits.data)
+kmeans = KMeans(n_clusters=10, random_state=0)
+X = load_digits().data
+clusters = kmeans.fit_predict(X)
+# print(kmeans.cluster_centers_)
+
+#Assigning Labels
+
+from scipy.stats import mode
+labels = np.zeros_like(clusters)
