c7958d44cfa4b70f4fda1f73421966fcc6abd2e8
ujs <ujalashanker@gmail.com>
2017-03-17 11:18:42 +0530

    SVM face recognition

--- SVMFace.py
+++ SVMFace.py
@@ -35,5 +35,25 @@ param_grid = {'svc__C': [1, 5, 10, 50],
               'svc__gamma': [0.0001, 0.0005, 0.001, 0.005]}
 grid = GridSearchCV(model, param_grid)
 
+# Fitting the model to our data!
 grid.fit(Xtrain, ytrain)
 print(grid.best_params_)
+
+#Predicting for test data
+model = grid.best_estimator_
+yfit = model.predict(Xtest)
+
+#Checking the model and seeing how we did
+from sklearn.metrics import classification_report
+print(classification_report(ytest, yfit,
+                            target_names=faces.target_names))
+
+
+# Confusion Matrix
+from sklearn.metrics import confusion_matrix
+mat = confusion_matrix(ytest, yfit)
+sns.heatmap(mat.T, square=True, annot=True, fmt='d', cbar=False,
+            xticklabels=faces.target_names,
+            yticklabels=faces.target_names)
+plt.xlabel('true label')
+plt.ylabel('predicted label')
